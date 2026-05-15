f53ec957e0c76df6396439fc9d417e7f65224e60
S-s26 <180008446+S-s26@users.noreply.github.com.>
2026-05-15 06:43:21 +0530

    new check

--- manage.py
+++ manage.py
@@ -15,6 +15,8 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+        print("ok")
+        print("added new thing")
     execute_from_command_line(sys.argv)
 
 
